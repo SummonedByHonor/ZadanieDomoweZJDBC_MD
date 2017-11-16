4bc0cf7e8b1fbb8ba649ed20fb386c97ce9bbfe4
MSI\Mateusz <mateuszdeptula1@gmail.com>
2017-11-16 22:16:11 +0100

    MainApp build and nextThree methods done

--- src/main/java/com/javabyd1/HomeworkApp/MainApp.java
+++ src/main/java/com/javabyd1/HomeworkApp/MainApp.java
@@ -1,12 +1,18 @@
 package com.javabyd1.HomeworkApp;
 
 import com.javabyd1.HomeworkApp.TableAdressesOperations.AdressesOperationsImpl;
+import com.javabyd1.HomeworkApp.TableCompanyOperations.CompanyOperationsImpl;
+import com.javabyd1.HomeworkApp.TableEmployeesOperations.EmployeesOperationsImpl;
+import com.javabyd1.HomeworkApp.TableJobOperations.JobOperationsImp;
 
 import java.sql.SQLException;
 import java.util.Scanner;
 
 public class MainApp {
     static AdressesOperationsImpl adressesOperations = new AdressesOperationsImpl();
+    static EmployeesOperationsImpl employeesOperations = new EmployeesOperationsImpl();
+    static CompanyOperationsImpl companyOperations = new CompanyOperationsImpl();
+    static JobOperationsImp jobOperations = new JobOperationsImp();
 
     public static void main(String[] args) {
 
@@ -32,17 +38,17 @@ public class MainApp {
                 String adresyMenuChoice = scanner.nextLine();
 
                 switch (adresyMenuChoice) {
-                    case "2": {
+                    case "1": {
                         try {
-                            adressesOperations.createNewRow();
+                            adressesOperations.getListOfRows();
                         } catch (SQLException e) {
                             e.printStackTrace();
                         }
                         break;
                     }
-                    case "1": {
+                    case "2": {
                         try {
-                            adressesOperations.getListOfRows();
+                            adressesOperations.createNewRow();
                         } catch (SQLException e) {
                             e.printStackTrace();
                         }
@@ -55,7 +61,7 @@ public class MainApp {
                             e.printStackTrace();
                         }
                     }
-                    case "4":{
+                    case "4": {
                         try {
                             adressesOperations.updateRow();
                         } catch (SQLException e) {
@@ -64,6 +70,113 @@ public class MainApp {
                     }
                 }
             }
+            case "2": {
+                System.out.println("Wybierz działanie:");
+                System.out.println("1. Wyświetl wszystkie rekordy.");
+                System.out.println("2. Dodaj rekord.");
+                System.out.println("3. Usuń rekord o określonym id.");
+                System.out.println("4. Zmodyfikuj rekord o określonym id.");
+                String employersMenuChoice = scanner.nextLine();
+
+                switch (employersMenuChoice) {
+                    case "2": {
+                        try {
+                            employeesOperations.createNewRow();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    }
+                    case "1": {
+                        try {
+                            employeesOperations.getListOfRows();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    }
+                    case "3": {
+                        try {
+                            employeesOperations.deleteRow();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    case "4": {
+                        try {
+                            employeesOperations.updateRow();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+            case "3": {
+                System.out.println("Wybierz działanie:");
+                System.out.println("1. Wyświetl wszystkie rekordy.");
+                System.out.println("2. Dodaj rekord.");
+                System.out.println("3. Usuń rekord o określonym id.");
+                System.out.println("4. Zmodyfikuj rekord o określonym id.");
+                String jobMenuChoice = scanner.nextLine();
+
+                switch (jobMenuChoice){
+                    case "1": {
+                        jobOperations.createNewRow();
+                    }
+                    case "2": {
+                        jobOperations.getListOfRows();
+                    }
+                    case "3": {
+                        jobOperations.deleteRow();
+                    }
+                    case "4": {
+                        jobOperations.updateRow();
+                    }
+                }
+            }
+            case "4": {
+                System.out.println("Wybierz działanie:");
+                System.out.println("1. Wyświetl wszystkie rekordy.");
+                System.out.println("2. Dodaj rekord.");
+                System.out.println("3. Usuń rekord o określonym id.");
+                System.out.println("4. Zmodyfikuj rekord o określonym id.");
+                String companyMenuChoice = scanner.nextLine();
+
+                switch (companyMenuChoice){
+                    case "1": {
+                        try {
+                            companyOperations.createNewRow();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    case "2": {
+                        try {
+                            companyOperations.getListOfRows();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    case "3": {
+                        try {
+                            companyOperations.deleteRow();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    case "4": {
+                        try {
+                            companyOperations.updateRow();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+            case "5": {
+                System.exit(0);
+            }
         }
     }
 }
+
--- src/main/java/com/javabyd1/HomeworkApp/TableEmployeesOperations/EmployeesOperationsImpl.java
+++ src/main/java/com/javabyd1/HomeworkApp/TableEmployeesOperations/EmployeesOperationsImpl.java
@@ -7,6 +7,8 @@ import java.sql.*;
 import java.util.List;
 import java.util.Scanner;
 
+import static javafx.scene.input.KeyCode.J;
+
 public class EmployeesOperationsImpl implements TableOperations {
     private Scanner scanner = new Scanner(System.in);
     private Scanner scannerChar = new Scanner(System.in);
@@ -66,21 +68,72 @@ public class EmployeesOperationsImpl implements TableOperations {
                 ", " + newEmployerCompanyId + ")";
 
         ResultSet resultSet = statement.executeQuery(sql);
-
+        statement.close();
+        connection.close();
     }
 
     @Override
-    public void deleteRow() {
-
+    public void deleteRow() throws SQLException {
+        Statement statement = getStatement();
+        System.out.println("Podaj id rekordu, który chcesz usunąć: ");
+        Integer choosenIdToDelete = scanner.nextInt();
+        sql = "DELETE FROM pracownicy WHERE id_pracownika = '" + choosenIdToDelete + "'";
+        int queryResult = statement.executeUpdate(sql);
+        statement.close();
+        connection.close();
+        System.out.println("Usunięto rekord o id " + choosenIdToDelete);
     }
 
     @Override
-    public void updateRow() {
-
+    public void updateRow() throws SQLException {
+        Statement statement = getStatement();
+        System.out.println("Podaj id rekordu, który chcesz zaktualizować.");
+        int chosenIdToUpdate = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Podaj nowy numer telefonu:");
+        String updatedTelephoneNumber = scanner.nextLine();
+        System.out.println("Podaj nowy adres email:");
+        String updatedEmailAdress = scanner.nextLine();
+        System.out.println("Podaj wynagrodzenie:");
+        Integer updatedSalaryValue = scanner.nextInt();
+        sql = "UPDATE pracownicy SET telefon = '" + updatedTelephoneNumber + "', email = '" + updatedEmailAdress +
+                "', wynagrodzenie = " + updatedSalaryValue;
+        Integer employerUpdate = statement.executeUpdate(sql);
+        statement.close();
+        connection.close();
     }
 
     @Override
-    public void getListOfRows() {
+    public void getListOfRows() throws SQLException {
+        Statement statement = getStatement();
+        sql = "SELECT * FROM pracownicy";
+        ResultSet resultQuerySet = statement.executeQuery(sql);
+        while (resultQuerySet.next()) {
+            int id = resultQuerySet.getInt("id_pracownika");
+            String name = resultQuerySet.getString("imie");
+            String lastName = resultQuerySet.getString("nazwisko");
+            String eyeColor = resultQuerySet.getString("kolor_oczu");
+            Integer height = resultQuerySet.getInt("wzrost");
+            String sex = resultQuerySet.getString("plec");
+            String telephoneNumber = resultQuerySet.getString("telefon");
+            String email = resultQuerySet.getString("email");
+            String PESELNumber = resultQuerySet.getString("PESEL");
+            Integer salary = resultQuerySet.getInt("wynagrodzenie");
 
+            System.out.println("ID pracownika: " + id);
+            System.out.println("Imię: " + name);
+            System.out.println("Nazwisko: " + lastName);
+            System.out.println("Kolor oczu: " + eyeColor);
+            System.out.println("Wzrost: " + height);
+            System.out.println("Płeć: " + sex);
+            System.out.println("Numer telefonu: " + telephoneNumber);
+            System.out.println("Adres email: " + email);
+            System.out.println("Numer PESEL: " + PESELNumber);
+            System.out.println("Aktualne wynagrodzenie pracownika: " + salary);
+            System.out.println("");
+        }
+        resultQuerySet.close();
+        statement.close();
+        connection.close();
     }
 }
